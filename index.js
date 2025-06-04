3a236889892612d43982fc24b8dfc505e7ecf200
KOO612 <ksm99007@gmail.com>
2025-06-04 17:56:59 +0900

    06/04 setHeader

--- index.js
+++ index.js
@@ -2,16 +2,45 @@ const http = require("http");
 
 const port = 3000;
 
+/*
+
 const server = http.createServer((req, res) => {
   // writeHead는 한 번만 호출되어야 함 end()가 호출되기 전에 호출되어야 함
   // 상태 코드는 응답 헤더를 클라이언트에 보냄
   res.writeHead(200, {
-    "Content-Type": "text/plain",
+    // content-type 응답 데이터의 형식을 알려줌
+    // "Content-Type": "text/plain",
+    // "Content-Type": "application/json",
+    "Content-Type": "text/html",
   });
   // 데이터가 로드되었음을 서버에 알림
-  res.end("Hello");
+  res.end("<h1>hello</h1>");
 });
+*/
 
+const server = http.createServer((req, res) => {
+  if (req.url == "/home") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
+        a: "a",
+        b: "b",
+      })
+    );
+  } else if (req.url == "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<h1>hello</h1>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
 server.listen(port, () => {
   console.log("server start");
 });
