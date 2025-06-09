12535a4293494a6c58f39161e70f5fca7e439fb5
KOO612 <ksm99007@gmail.com>
2025-06-09 15:51:28 +0900

    06/09 refresh 토큰 발급 및 cookie httponly

--- part05/app.js
+++ part05/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
 const port = 3000;
+const cookieParser = require("cookie-parser");
 
 const secretText = "secret";
 
@@ -16,15 +17,51 @@ const posts = [
   },
 ];
 
+let refreshTokens = [];
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const user = { name: username };
 
   // 토큰 생성
-  const acceseToken = jwt.sign(user, secretText);
-  res.json({ acceseToken: acceseToken });
+  // 유효기간 추가
+  const accesseToken = jwt.sign(user, secretText, { expiresIn: "30s" });
+
+  // refreshToken 생성
+  const refreshToken = jwt.sign(user, "refresh", { expiresIn: "1d" });
+
+  refreshTokens.push(refreshToken);
+
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
+  res.json({ accesseToken: accesseToken });
+});
+
+// refresh 토큰 검증
+app.get("/refresh", (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) {
+    return res.sendStatus(401);
+  }
+
+  const refreshToken = cookies.jwt;
+  if (!refreshTokens.includes(refreshToken)) {
+    return res.sendStatus(403);
+  }
+
+  jwt.verify(refreshToken, "refresh", (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    const accesseToken = jwt.sign({ name: user.name }, secretText, { expiresIn: "30s" });
+    res.json({ accesseToken });
+  });
 });
 
 app.get("/posts", authMiddleware, (req, res) => {
